26522fb1eac574e5e9ace0503b01c86ef6068bdb
AJTimePyro <ajtimepyro@gmail.com>
2021-10-22 16:34:59 +0530

    nothing

--- plugins/downloader/ytDL.py
+++ plugins/downloader/ytDL.py
@@ -25,38 +25,6 @@ class YTDown:
         self.event = event
         self.url = url
         self.log_obj = log_object
-        # self.tmpVideoDownload = f'{downloadFolder}\\tmp\\video\\'
-        self.tmpVideoDownload = f'{downloadFolder}/tmp/video/'
-        # self.tmpAudioDownload = f'{downloadFolder}\\tmp\\audio\\'
-        self.tmpAudioDownload = f'{downloadFolder}/tmp/audio/'
-    
-    # async def downAudio(self):
-    #     files_before = listdir(self.tmpAudioDownload)
-    #     listAudio = {quality.abr[:-4] : quality.itag for quality in self.qualities if quality.mime_type == 'audio/webm'}
-    #     sizeA = (int(i) for i in listAudio)
-    #     stream = self.yt.streams.get_by_itag(listAudio[f'{max(sizeA)}'])
-    #     stream.download(output_path = self.tmpAudioDownload)
-    #     files_after = listdir(self.tmpAudioDownload)
-    #     try:
-    #         audioFile = str([i for i in files_after if i not in files_before][0])
-    #     except IndexError:
-    #         print('audio file not downloaded')
-    #     else:
-    #         await self.merge(audioFile, self.videoFile)
-    #     return
-
-    # async def merge(self, audioFile, videoFile):
-    #     videopath = f'{self.tmpVideoDownload}{videoFile}'
-    #     audiopath = f'{self.tmpAudioDownload}{audioFile}'
-    #     videoclip = VideoFileClip(videopath)
-    #     audioclip = AudioFileClip(audiopath)
-    #     new_audioclip = CompositeAudioClip([audioclip])
-    #     videoclip.audio = new_audioclip
-    #     videoclip.write_videofile(f'{downloadFolder}{videoFile}')
-    #     self.filename = videoFile
-    #     remove(videopath)
-    #     remove(audiopath)
-    #     return
 
     async def start(self):
 
@@ -78,7 +46,6 @@ class YTDown:
             self.bot.loop.create_task(edit_msg(progress_bar, percentage, completed, speed, remaining))
         try:
             self.yt = YouTube(self.url, on_progress_callback = progress_function)
-            # self.qualities = self.yt.streams.filter(adaptive = True)   #Filtering Streams Having Audio & Video
             self.qualities = self.yt.streams.filter(progressive = True)   #Filtering Streams Having Audio & Video
         except exceptions.VideoUnavailable:
             task("No Task")
@@ -109,7 +76,7 @@ class YTDown:
 
                     #Getting String Value From event.data
                     itag = event.data.decode('utf-8')
-                    files_before = listdir(self.tmpVideoDownload)
+                    files_before = listdir(downloadFolder)
                     stream = self.yt.streams.get_by_itag(itag)
 
                     #Trying To Download Video To Server
@@ -117,10 +84,10 @@ class YTDown:
                         self.msg = await self.bot.edit_message(self.bmsg, starting_to_download, parse_mode = 'html')
                         global t1
                         t1 = time()
-                        stream.download(output_path = self.tmpVideoDownload)
+                        stream.download(output_path = downloadFolder)
                     except Exception as e:
                         task("No Task")
-                        files_after = listdir(self.tmpVideoDownload)
+                        files_after = listdir(downloadFolder)
                         await self.bot.send_message(dev, f'In ytDL.py {line_number()} {e}')
                         await self.bot.edit_message(self.msg, unsuccessful_upload, parse_mode = 'html')
                         try:
@@ -129,9 +96,9 @@ class YTDown:
                             pass
                         else:
                             #Deleting Incomplete File
-                            remove(f'{self.tmpVideoDownload}{videoFile}')
+                            remove(f'{downloadFolder}{videoFile}')
                     else:
-                        files_after = listdir(self.tmpVideoDownload)
+                        files_after = listdir(downloadFolder)
                         try:
                             videoFile = str([i for i in files_after if i not in files_before][0])
                         except IndexError:
@@ -146,7 +113,7 @@ class YTDown:
                             await Upload.start(self.videoFile, self.log_obj, self.bot, n_msg, event.sender_id)
                             return True
                    
-                await asyncio.sleep(60)
+                # await asyncio.sleep(60)
                 return
             else:
                 await self.bot.edit_message(self.process_msg, all_above_limit, parse_mode = 'html')
